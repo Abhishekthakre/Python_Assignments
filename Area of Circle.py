3724c357ed8de644f43f754e2ddd6e4f3ca17fca
unknown <abhishekthakre42@gmail.com>
2023-02-10 13:48:04 +0530

    reinitialized Files

---
+++ Area of Circle.py
@@ -0,0 +1,7 @@
+# r=12
+# area=3.14 * (r*r)
+# print('Radius =',r, 'Area of Circle=',area)
+a=int(input("Enter A  Number :")
+if a>0:
+    print("Positive")
+print("Non Positive....")
---
+++ Result.py
@@ -0,0 +1,12 @@
+m=int(input("Enter the Number :"))
+if 90<m<=100:
+    print("A")
+elif 75<m<=90:
+    print("B")
+elif 60<m<=75:
+    print("c")
+elif 35<m<=60:
+    print("D")
+else:
+    print("FAIL")
+
